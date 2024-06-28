a7d9de17668d17b6cb5574e27ce51f04f184bea9
gopals <gopalsuthar@mirafra.com>
2024-06-28 19:15:43 +0530

    updated hdl top and base test write master side

--- src/axi4LiteMasterVIP/src/axi4LiteMasterWriteVIP/src/tb/Axi4LiteMasterWriteHdlTop.sv
+++ src/axi4LiteMasterVIP/src/axi4LiteMasterWriteVIP/src/tb/Axi4LiteMasterWriteHdlTop.sv
@@ -33,15 +33,6 @@ module Axi4LiteMasterWriteHdlTop;
                                                             .aresetn(aresetn)
                                                           );
                                                           
-/*  Axi4LiteMasterWriteInterface axi4LiteMasterWriteInterface(.aclk(aclk),
-                                                            .aresetn(aresetn),
-                                                            .awready(1'b1),
-                                                            .wready(1'b1),
-                                                            .bvalid(1'b1),
-                                                            .bresp(2'b00)
-                                                          );
-*/
-
   initial begin
     axi4LiteMasterWriteInterface.awready = 1'b1;  
     axi4LiteMasterWriteInterface.wready  = 1'b1;
@@ -52,20 +43,7 @@ module Axi4LiteMasterWriteHdlTop;
   genvar i;
   generate
     for (i=0; i<NO_OF_WRITEMASTERS; i++) begin : Axi4LiteMasterWriteAgentBFM
-      Axi4LiteMasterWriteAgentBFM #() axi4LiteMasterWriteAgentBFM(.aclk(axi4LiteMasterWriteInterface.aclk),
-                                                                  .aresetn(axi4LiteMasterWriteInterface.aresetn),
-                                                                  .awvalid(axi4LiteMasterWriteInterface.awvalid),
-                                                                  .awready(axi4LiteMasterWriteInterface.awready),
-                                                                  .awaddr(axi4LiteMasterWriteInterface.awaddr),
-                                                                  .awprot(axi4LiteMasterWriteInterface.awprot),
-                                                                  .wvalid(axi4LiteMasterWriteInterface.wvalid),
-                                                                  .wready(axi4LiteMasterWriteInterface.wready),
-                                                                  .wdata(axi4LiteMasterWriteInterface.wdata),
-                                                                  .wstrb(axi4LiteMasterWriteInterface.wstrb),
-                                                                  .bvalid(axi4LiteMasterWriteInterface.bvalid),
-                                                                  .bready(axi4LiteMasterWriteInterface.bready),
-                                                                  .bresp(axi4LiteMasterWriteInterface.bresp)
-                                                                 );
+      Axi4LiteMasterWriteAgentBFM #() axi4LiteMasterWriteAgentBFM(axi4LiteMasterWriteInterface);
     end
   endgenerate
 
