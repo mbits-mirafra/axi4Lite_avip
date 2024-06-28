0c16641bf35078c732cdc72142138350224814cd
gopals <gopalsuthar@mirafra.com>
2024-06-29 01:00:45 +0530

    updated .f file in master read side

--- src/axi4LiteMasterVIP/src/axi4LiteMasterReadVIP/sim/Axi4LiteReadMasterCompile.f
+++ src/axi4LiteMasterVIP/src/axi4LiteMasterReadVIP/sim/Axi4LiteReadMasterCompile.f
@@ -7,12 +7,12 @@
 +incdir+../../src/tb/test/
 
 ../../src/Axi4LiteReadMasterGlobalPkg.sv
+../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadAssertCoverParameter.sv
 ../../src/masterReadAgent/masterReadAgent/Axi4LiteMasterReadPkg.sv
 ../../src/masterReadAgent/masterReadAgent/masterReadSequences/Axi4LiteMasterReadSeqPkg.sv
 ../../src/masterReadEnv/Axi4LiteReadMasterEnvPkg.sv
 ../../src/tb/test/Axi4LiteReadMasterTestPkg.sv
 ../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadInterface.sv
-../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadAssertCoverParameter.sv
 ../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadAssertions.sv
 ../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadCoverProperty.sv
 ../../src/masterReadAgent/masterReadAgentBFM/Axi4LiteMasterReadDriverBFM.sv
