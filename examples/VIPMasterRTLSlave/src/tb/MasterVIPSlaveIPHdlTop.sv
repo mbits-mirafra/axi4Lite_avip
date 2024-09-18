7239ce24b4a0d5d57f059077eef6ca697ef7e32d
gopals <gopalsuthar@mirafra.com>
2024-09-18 13:15:43 +0530

    updated hdl top

--- examples/VIPMasterRTLSlave/src/tb/MasterVIPSlaveIPHdlTop.sv
+++ examples/VIPMasterRTLSlave/src/tb/MasterVIPSlaveIPHdlTop.sv
@@ -1,6 +1,9 @@
 `ifndef MASTERVIPSLAVEIPHDLTOP_INCLUDED_
 `define MASTERVIPSLAVEIPHDLTOP_INCLUDED_
 
+`define AXI4LITE_MASTERWRITEINTERFACE axi4LiteInterface.axi4LiteMasterInterface.axi4LiteMasterWriteInterface  
+`define AXI4LITE_MASTERREADINTERFACE axi4LiteInterface.axi4LiteMasterInterface.axi4LiteMasterReadInterface  
+
 module MasterVIPSlaveIPHdlTop;
 
   import uvm_pkg::*;
@@ -29,11 +32,40 @@ module MasterVIPSlaveIPHdlTop;
     end
     aresetn = 1'b1;
   end
+
   MasterVIPSlaveIPInterface masterVIPSlaveIPInterface(.aclk(aclk),
                                                       .aresetn(aresetn)
                                                      );
 
+  slave #(.ADDRESS_WIDTH(Axi4LiteWriteMasterGlobalPkg::ADDRESS_WIDTH), 
+          .DATA_WIDTH(Axi4LiteWriteMasterGlobalPkg::DATA_WIDTH))
+  slaveRTL(.clk(aclk),
+           .rst_n(aresetn)
+          );
+
   initial begin
+    assign slaveRTL.awvalid = `AXI4LITE_MASTERWRITEINTERFACE.awvalid;
+    assign slaveRTL.awaddr  = `AXI4LITE_MASTERWRITEINTERFACE.awaddr;
+    assign slaveRTL.awprot  = `AXI4LITE_MASTERWRITEINTERFACE.awprot;
+    assign slaveRTL.wvalid  = `AXI4LITE_MASTERWRITEINTERFACE.wvalid;
+    assign slaveRTL.wdata   = `AXI4LITE_MASTERWRITEINTERFACE.wdata;
+    assign slaveRTL.wstrb   = `AXI4LITE_MASTERWRITEINTERFACE.wstrb;
+    assign slaveRTL.bready  = `AXI4LITE_MASTERWRITEINTERFACE.bready;
+
+    assign `AXI4LITE_MASTERWRITEINTERFACE.awready = slaveRTL.awready;
+    assign `AXI4LITE_MASTERWRITEINTERFACE.wready  = slaveRTL.wready;
+    assign `AXI4LITE_MASTERWRITEINTERFACE.bvalid  = slaveRTL.bvalid;
+    assign `AXI4LITE_MASTERWRITEINTERFACE.bresp   = slaveRTL.bresp;
+
+    assign slaveRTL.arvalid = `AXI4LITE_MASTERREADINTERFACE.arvalid;
+    assign slaveRTL.araddr  = `AXI4LITE_MASTERREADINTERFACE.araddr;
+    assign slaveRTL.arprot  = `AXI4LITE_MASTERREADINTERFACE.arprot;
+    assign slaveRTL.rready  = `AXI4LITE_MASTERREADINTERFACE.rready;
+
+    assign `AXI4LITE_MASTERREADINTERFACE.arready = slaveRTL.arready;
+    assign `AXI4LITE_MASTERREADINTERFACE.rvalid  = slaveRTL.rvalid;
+    assign `AXI4LITE_MASTERREADINTERFACE.rdata   = slaveRTL.rdata;
+    assign `AXI4LITE_MASTERREADINTERFACE.rresp   = slaveRTL.rresp;
   end
 
   genvar i;
