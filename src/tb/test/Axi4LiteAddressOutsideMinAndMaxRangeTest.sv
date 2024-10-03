7dfdbfb3407c6f68e43a88ffd3d08bf3044b60a5
mahadevaswamy05 <mahadevaswamy@mirafra.com>
2024-10-03 22:09:16 +0530

    updated the address out of range vseq and testcase

--- src/tb/test/Axi4LiteAddressOutsideMinAndMaxRangeTest.sv
+++ src/tb/test/Axi4LiteAddressOutsideMinAndMaxRangeTest.sv
@@ -7,6 +7,11 @@ class Axi4LiteAddressOutsideMinAndMaxRangeTest extends Axi4LiteBaseTest;
   Axi4LiteVirtualAddressOutsideMinAndMaxRangeSeq axi4LiteVirtualAddressOutsideMinAndMaxRangeSeq;
   
   extern function new(string name = "Axi4LiteAddressOutsideMinAndMaxRangeTest", uvm_component parent = null);
+ 
+  extern function void setupAxi4LiteMasterWriteAgentConfig();
+  extern function void setupAxi4LiteMasterReadAgentConfig();
+  extern function void setupAxi4LiteSlaveWriteAgentConfig();
+  extern function void setupAxi4LiteSlaveReadAgentConfig();
   extern virtual task run_phase(uvm_phase phase);
 
 endclass : Axi4LiteAddressOutsideMinAndMaxRangeTest
@@ -15,6 +20,38 @@ function Axi4LiteAddressOutsideMinAndMaxRangeTest::new(string name = "Axi4LiteAd
                                  uvm_component parent = null);
   super.new(name, parent);
 endfunction : new
+
+function void Axi4LiteAddressOutsideMinAndMaxRangeTest::setupAxi4LiteMasterWriteAgentConfig();
+  super.setupAxi4LiteMasterWriteAgentConfig();
+  foreach(axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteWriteMasterEnvConfig.axi4LiteMasterWriteAgentConfig[i]) begin
+  axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteWriteMasterEnvConfig.axi4LiteMasterWriteAgentConfig[i].minAddressRange = 1'b1;
+  axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteWriteMasterEnvConfig.axi4LiteMasterWriteAgentConfig[i].maxAddressRange = 8'hff;
+end 
+endfunction
+
+function void Axi4LiteAddressOutsideMinAndMaxRangeTest::setupAxi4LiteMasterReadAgentConfig();
+  super.setupAxi4LiteMasterReadAgentConfig();
+  foreach(axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteReadMasterEnvConfig.axi4LiteMasterReadAgentConfig[i]) begin
+  axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteReadMasterEnvConfig.axi4LiteMasterReadAgentConfig[i].minAddressRange = 1'b1;
+  axi4LiteEnvConfig.axi4LiteMasterEnvConfig.axi4LiteReadMasterEnvConfig.axi4LiteMasterReadAgentConfig[i].maxAddressRange = 8'hff;
+end 
+endfunction
+
+function void Axi4LiteAddressOutsideMinAndMaxRangeTest::setupAxi4LiteSlaveWriteAgentConfig();
+  super.setupAxi4LiteSlaveWriteAgentConfig();
+  foreach(axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteWriteSlaveEnvConfig.axi4LiteSlaveWriteAgentConfig[i]) begin
+  axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteWriteSlaveEnvConfig.axi4LiteSlaveWriteAgentConfig[i].minAddressRange = 1'b1;
+  axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteWriteSlaveEnvConfig.axi4LiteSlaveWriteAgentConfig[i].maxAddressRange = 8'hff;
+end 
+endfunction
+
+function void Axi4LiteAddressOutsideMinAndMaxRangeTest::setupAxi4LiteSlaveReadAgentConfig();
+  super.setupAxi4LiteSlaveReadAgentConfig();
+  foreach(axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteReadSlaveEnvConfig.axi4LiteSlaveReadAgentConfig[i]) begin
+  axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteReadSlaveEnvConfig.axi4LiteSlaveReadAgentConfig[i].minAddressRange = 1'b1;
+  axi4LiteEnvConfig.axi4LiteSlaveEnvConfig.axi4LiteReadSlaveEnvConfig.axi4LiteSlaveReadAgentConfig[i].maxAddressRange = 8'hff;
+end 
+endfunction
 
 task Axi4LiteAddressOutsideMinAndMaxRangeTest::run_phase(uvm_phase phase);
 
