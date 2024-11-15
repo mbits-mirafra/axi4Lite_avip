5eb8e485172f8248279e724ef4128f0f97c0e8e7
gopals <gopalsuthar@mirafra.com>
2024-11-15 13:36:01 +0530

    seperated both python files from regression file

--- examples/VIPMasterRTLSlave/sim/questasim/regression_handling.py
+++ examples/VIPMasterRTLSlave/sim/questasim/regression_handling.py
@@ -7,6 +7,7 @@ import sys
 import os
 import time
 from datetime import datetime
+from errorFatalChecks import write_into_error_file
 
 ## Variables
 reg_list_array = []
@@ -38,34 +39,6 @@ with open(path,'r') as f:
 outputLogFile = "featureViolatedByRTLReport.log"
 open(outputLogFile, "w").close()
 
-def write_into_error_file(logFile, simulationLogFile):
-    errorKeywords = ["ERROR","Error","FATAL"]
-    errorFound = []
-
-    with open(simulationLogFile,"a") as simulationFile:
-        for logFilePath in logFile:
-            try:
-                with open(logFilePath, "r") as file:
-                    simulationFile.write("\n--- Errors from log file: {} ---\n".format(logFilePath))
-                    for lineNumber, line in enumerate(file, start = 1):
-                        if any(keyword in line for keyword in errorKeywords):
-                            errorLine = "{} - Line {}: {}".format(logFilePath, lineNumber, line.strip())
-                            errorFound.append(errorLine)
-                            simulationFile.write(errorLine + "\n")
-
-            except FileNotFoundError:
-                print("Error: The file {} was not found".format(logFilePath))
-                simulationFile.write("Error: The file {} was not found\n".format(logFilePath))
-            except Exception as e:
-                print("An error occurred while reading {}: {}".format(logFilePath, e))
-                simulationFile.write("An error occurred while reading {}: {}\n".format(logFilePath, e))
-
-        if not errorFound:
-            simulationFile.write("Errors Found in the log file:\n")
-            print("No errors found in the log files")
-        else:
-            print("Errors written to {}".format(simulationLogFile))
-
 for testname in reg_list_array:
   timestr = time.strftime( "%d%m%Y-%H%M%S" )
   if debug:
@@ -80,6 +53,4 @@ for testname in reg_list_array:
 
   logFile = ["{}/{}.log".format(test_folder, testname)] 
   write_into_error_file(logFile,"featureViolatedByRTLReport.log")
-
-
 
